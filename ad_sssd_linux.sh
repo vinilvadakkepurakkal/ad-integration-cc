2b5c001b17a6d4ef67d2efb86419433efff99b4b
Vinil Vadakkepurakkal <46518879+vinilvadakkepurakkal@users.noreply.github.com>
2023-01-11 14:38:41 +0530

    update 1

--- ad_sssd_linux.sh
+++ ad_sssd_linux.sh
@@ -4,8 +4,7 @@
 #OS Tested : CentOS 7 / RHEL7
 #Env - Azure
 #Installing the required Packages
-yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools \ 
-krb5-workstation openldap-clients policycoreutils-python-utils -y
+yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python-utils -y
 
 #define variables for AD 
 AD_SERVER=<ad server use_fully_qualified_names>
